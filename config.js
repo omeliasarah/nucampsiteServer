b2d792cf0b66cbb6216ad9b152646ba3dbdad411
Sarah Omelia <omelia.sarah20@gmail.com>
2021-04-29 20:02:33 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/nucampsite",
-};
+  'secretKey': '12345-67890-09876-54321',
+  'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+  'facebook': {
+      clientId: '424384358962712',
+      clientSecret: '027b420fcb717998fd575de9a1216434'
+  }
+}
